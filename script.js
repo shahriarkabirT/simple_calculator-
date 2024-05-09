eabccc73a132fda100498aa632290642a9941d8f
shahriar <shahriarkabir078@gmail.com>
2024-05-09 17:55:47 +0600

    new

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+let resultflag = 0;
+function appendToDisplay(value){
+    if(resultflag == 0){
+        document.getElementById('display').value += value;
+    }
+    else{
+        resultflag = 0;
+        document.getElementById('display').value = '';
+        document.getElementById('display').value += value;
+    }
+
 }
 
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
-function calculate() {
-    var result = eval(document.getElementById('display').value);
-    document.getElementById('display').value = result;
-}
+    function calculate() {
+        resultflag = 1;
+        try{
+            var result = eval(document.getElementById('display').value);
+            if(result != 'Infinity'){
+                document.getElementById('display').value = result;
+            }
+            else if(result == 'Infinity'){
+                document.getElementById('display').value = 'undefined';
+            }
+        console.log(result);
+         }
+        catch(e){
+            document.getElementById('display').value = "Error";
+        }
+    }
+
+
 function backspace() {
     var display = document.getElementById('display');
     display.value = display.value.slice(0, -1); 
